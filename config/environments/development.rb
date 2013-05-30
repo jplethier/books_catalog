d312075a5f15903b2933c24a2a286ced8419599c
João Paulo Lethier <jplethier@gmail.com>
2013-05-30 17:26:29 -0300

    configurando letter_opener e host para devise user para envio de emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,13 @@ BooksCatalog::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_controller.action_on_unpermitted_parameters = :raise
 end
